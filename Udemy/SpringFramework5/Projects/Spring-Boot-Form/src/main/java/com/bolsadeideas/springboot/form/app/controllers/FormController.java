c146615133cd708b64b50c39c6384e408f01e79a
Fherney Adrian Silva Buitrago <fhersil_13@hotmail.com>
2022-10-01 23:28:43 -0500

    Validación de errores normal y personalizados
    Se validaron errores a través del messages.properties y se usaron diferentes anotaciones (@SessionAttribute con el parámetro el objeto usuario y la clase SessionStatus) para mantener la persistencia de los datos y el control de estos errores a través de expresiones regulares en el form.html.

--- Udemy/SpringFramework5/Projects/Spring-Boot-Form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ Udemy/SpringFramework5/Projects/Spring-Boot-Form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -7,22 +7,28 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
 		Usuario usuario = new Usuario();
+		usuario.setId("1.037.668.059");
+		usuario.setNombre("Fherney");
+		usuario.setApellido("Silva");
 		model.addAttribute("titulo", "Formulario Usuarios");
 		model.addAttribute("usuario", usuario);
 		return "form";
 	}
 	
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
 		model.addAttribute("titulo", "Resultado Formulario Usuarios");
 		
 		/*Map<String, String> errores = new HashMap<>();
@@ -32,7 +38,7 @@ public class FormController {
 		});
 		model.addAttribute("error", errores);*/
 		if(result.hasErrors()) return "form";
-		 
+		status.setComplete();
 		model.addAttribute("usuario", usuario);
 		return "resultado";
 	}
--- Udemy/SpringFramework5/Projects/Spring-Boot-Form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ Udemy/SpringFramework5/Projects/Spring-Boot-Form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,19 +1,33 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
 public class Usuario {
 	
+	@Pattern(regexp = "[1-9]{1}[.][\\d]{3}[.][0-9]{3}[.][0-9]{3}")
+	private String id;
+	
 	@NotEmpty
+	private String nombre;
+	
+	@NotEmpty
+	private String apellido;
+	
+	@NotEmpty
+	@Size(min = 3, max = 8)
 	private String username;
 	
 	@NotEmpty
 	private String password;
 	
 	@NotEmpty
+	@Email
 	private String email;
 
 }
