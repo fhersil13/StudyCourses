07dfe75a728a855ed171871f15207da86f6654ea
Fherney Adrian Silva Buitrago <fhersil_13@hotmail.com>
2022-10-03 16:36:45 -0500

    Validación de errores a través de una clase
    Se creó una clase que implementa una interfaz de Spring llamada Validator en donde se personalizan y controlan los errores del formulario.

--- Udemy/SpringFramework5/Projects/Spring-Boot-Form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ Udemy/SpringFramework5/Projects/Spring-Boot-Form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -2,19 +2,31 @@ package com.bolsadeideas.springboot.form.app.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+import com.bolsadeideas.springboot.form.app.validation.UsuarioValidador;
 
 @Controller
 @SessionAttributes("usuario")
 public class FormController {
+	
+	@Autowired
+	private UsuarioValidador validador;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(validador);
+	}
 
 	@GetMapping("/form")
 	public String form(Model model) {
@@ -29,6 +41,8 @@ public class FormController {
 	
 	@PostMapping("/form")
 	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
+//		validador.validate(usuario, result);
+		
 		model.addAttribute("titulo", "Resultado Formulario Usuarios");
 		
 		/*Map<String, String> errores = new HashMap<>();
--- Udemy/SpringFramework5/Projects/Spring-Boot-Form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ Udemy/SpringFramework5/Projects/Spring-Boot-Form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,8 +1,9 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+//import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -10,17 +11,17 @@ import lombok.Data;
 @Data
 public class Usuario {
 	
-	@Pattern(regexp = "[1-9]{1}[.][\\d]{3}[.][0-9]{3}[.][0-9]{3}")
+//	@Pattern(regexp = "[1-9]{1}[.][\\d]{3}[.][0-9]{3}[.][0-9]{3}")
 	private String id;
 	
-	@NotEmpty
+//	@NotEmpty
 	private String nombre;
 	
 	@NotEmpty
 	private String apellido;
 	
-	@NotEmpty
-	@Size(min = 3, max = 8)
+	@NotBlank
+	@Size(min = 3, max = 9)
 	private String username;
 	
 	@NotEmpty
