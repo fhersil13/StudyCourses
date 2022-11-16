7fd003a9846b4d8c850f4ee9161a4b077a5ef21e
Fherney Silva <fhersil_13@hotmail.com>
2022-11-16 00:08:28 -0500

    Spring MVC & Data JPA Crud Completo
    Se creó un proyecto desde cero con paquetes entity, dao, services, controller con db h2 y MySQL Server y MySQL Workbench usando Thymeleaf como front.

---
+++ Udemy/SpringFramework5/Projects/Spring-Boot-DataJPA/src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,9 @@
+package com.bolsadeideas.springboot.app.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
+	
+}
